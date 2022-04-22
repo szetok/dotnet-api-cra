a051ea7409349ec1e9ce0c9e092959577de8b8fa
seetoe <kevinseetoe@gmail.com>
2022-04-22 14:01:04 -0400

    fix casing on react app files to not have uppercase

--- src/clientapp/src/index.js
+++ src/clientapp/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './app';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/index.css';
