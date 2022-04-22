9e631d7f980643453727c8f97a3c3c90e6a11fc1
seetoe <kevinseetoe@gmail.com>
2022-04-22 17:42:24 -0400

    move clientapp back into src directory and set up .net project to run react app with SPA proxy

--- src/Program.cs
+++ src/Program.cs
@@ -2,27 +2,23 @@ using DotnetApiCra.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddTransientServices();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseHsts();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
